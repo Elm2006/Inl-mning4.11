0689e9a10c4d91fbffa9e8f2d00cc5575968970c
isel06002 <isel06002@E-PW0234HX>
2023-10-20 13:19:19 +0200

    Add project files.

---
+++ Inlämning4.11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Inlämning_4.__11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett ord");
+            string ord = Console.ReadLine();
+            string gånger = "";
+            for (int i = 0; i < ord.Length; i++)
+            {
+                for (int j = 0; j != i && j < ord.Length; j++)
+                {
+                    if (ord[i] == ord[j] && !gånger.Contains(ord[i]))
+                    {
+                        Console.WriteLine(ord[j] + " Förekommer mer än 1 gång");
+                        gånger += ord[j];
+                    }
+                }
+            }
+        }
+    }
+}
